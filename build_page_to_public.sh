3ab76d7acaa3e32b3174f882d60e67b67c91da06
jiao <greed@JiaodeMacBook-Pro.local>
2020-02-25 18:24:22 +0800

    base url fix

--- build_page_to_public.sh
+++ build_page_to_public.sh
@@ -1,1 +1,3 @@
-hugo --theme=hugo-journal  --baseUrl="http://jlab.github.io/"
+# hugo --theme=hugo-journal  --baseUrl="http://jlab.github.io/"
+
+hugo --theme=hugo-journal  --baseUrl="http://www.jlab.tech/"
